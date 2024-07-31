7e96019b991a535744a2fe8ce14bae0718cafd80
Arda Doğrul <darda159@gmail.com>
2024-07-31 14:59:12 +0300

    updating some things according to my head

---
+++ Headers/RabinKarp.hpp
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <limits.h>
+#include "Methods.hpp"
+#define d 256
+
+/* pat -> pattern
+	txt -> text
+	q -> A prime number
+*/
+void search(string keywords, string filePath, int q)
+{
+    char* pat = (char*)keywords.c_str();
+	int M = strlen(keywords.c_str());
+	int N = get_file_size(filePath);
+    char *txt = ReadAllBytes(filePath);
+
+	
+	int i, j;
+	int p = 0; // hash value for pattern
+	int t = 0; // hash value for txt
+	int h = 1;
+
+	// The value of h would be "pow(d, M-1)%q"
+	for (i = 0; i < M - 1; i++)
+		h = (h * d) % q;
+
+	// Calculate the hash value of pattern and first
+	// window of text
+	for (i = 0; i < M; i++) {
+		p = (d * p + pat[i]) % q;
+		t = (d * t + txt[i]) % q;
+	}
+
+	// Slide the pattern over text one by one
+	for (i = 0; i <= N - M; i++) {
+
+		// Check the hash values of current window of text
+		// and pattern. If the hash values match then only
+		// check for characters one by one
+		if (p == t) {
+			/* Check for characters one by one */
+			for (j = 0; j < M; j++) {
+				if (txt[i + j] != pat[j]) {
+					break;
+				}
+			}
+
+			// if p == t and pat[0...M-1] = txt[i, i+1,
+			// ...i+M-1]
+
+			if (j == M)
+				cout << "Pattern found at index " << i
+					<< endl;
+		}
+
+		// Calculate hash value for next window of text:
+		// Remove leading digit, add trailing digit
+		if (i < N - M) {
+			t = (d * (t - txt[i] * h) + txt[i + M]) % q;
+
+			// We might get negative value of t, converting
+			// it to positive
+			if (t < 0)
+				t = (t + q);
+		}
+	}
+}
